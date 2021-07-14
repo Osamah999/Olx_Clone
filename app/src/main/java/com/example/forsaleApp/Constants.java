1d904390bec3aacf86d62b9086ac373d154a57b8
Osamah999 <osamahalhammadi@gmail.com>
2021-07-15 04:50:53 +0530

    Add extra files

---
+++ app/src/main/java/com/example/forsaleApp/Constants.java
@@ -0,0 +1,19 @@
+package com.example.forsaleApp;
+
+public class Constants {
+
+    //product categories
+    public static final String[] productCategories =
+            {
+                "Cars",
+                "Properties",
+                "Mobiles",
+                "Jobs",
+                "Bikes",
+                "Electronics",
+                "Laptops",
+                "Furniture",
+                "Service",
+                "Others"
+            };
+}
